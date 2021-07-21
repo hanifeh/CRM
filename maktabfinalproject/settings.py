682ffaea0ef26e7c2814da086ec2c56979d67ee2
sadegh <sadeghhanifehnia@gmail.com>
2021-07-21 14:17:50 +0430

    change database to postgres

--- maktabfinalproject/settings.py
+++ maktabfinalproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'maktabfinalproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maktabfinalproject',
+        'USER': 'sadegh',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
