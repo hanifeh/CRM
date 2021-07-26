0005e03d6233c7a219128c7826d3e2c757fd4ddf
sadegh <sadeghhanifehnia@gmail.com>
2021-07-26 11:02:19 +0430

    add debug toolbar

--- maktabfinalproject/settings.py
+++ maktabfinalproject/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'product',
     'organization',
     'followup',
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'maktabfinalproject.urls'
@@ -132,12 +134,10 @@ MEDIA_ROOT = BASE_DIR / 'uploads'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = (
-#     ("js", os.path.join(STATIC_ROOT, 'js')),
-#     ("css", os.path.join(STATIC_ROOT, 'css')),
-#     ("images", os.path.join(STATIC_ROOT, 'images')),
-# )
-
 LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
--- maktabfinalproject/urls.py
+++ maktabfinalproject/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -26,6 +27,7 @@ urlpatterns = [
     path('organizations/', include('organization.urls')),
     path('products/', include('product.urls')),
     path('followup/', include('followup.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
