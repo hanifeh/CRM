f67fd1fa867606394fa1a6ee2f58a863f96492d1
sadegh <sadeghhanifehnia@gmail.com>
2021-07-27 11:46:02 +0430

    clear front

--- quote/views.py
+++ quote/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
@@ -23,13 +24,19 @@ class ViewCreateQuote(LoginRequiredMixin, TemplateView):
     def post(self, *args, **kwargs):
         formset = QuoteItemCreateFormSet(data=self.request.POST)
         if formset.is_valid():
-            quote = Quote.objects.create(creator=self.request.user,
-                                         organization=Organization.objects.get(pk=self.request.POST['organization']))
+            organization = Organization.objects.get(pk=self.request.POST['organization'])
+            quote = Quote.objects.create(creator=self.request.user, organization=organization)
             for form in formset:
                 form.instance.quote = quote
                 try:
                     form.save()
                 except:
-                    pass
-            return redirect(reverse_lazy("home"))
-        return self.render_to_response({'form': formset})
+                    quote.delete()
+                    messages.error(self.request, 'Invalid input.')
+                    return redirect(reverse_lazy('quote:create-quote'))
+            messages.success(self.request, 'Quote create successfully.')
+            return redirect(reverse_lazy("quote:create-quote"))
+        else:
+            messages.error(self.request, 'Invalid input.')
+            return redirect(reverse_lazy('quote:create-quote'))
+        # return self.render_to_response({'form': formset})
