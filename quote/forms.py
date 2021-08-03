2ea1e21c9f90cce6bdd42335124c9ac0667bcf11
sadegh <sadeghhanifehnia@gmail.com>
2021-08-03 16:32:12 +0430

    update tasks

--- quote/forms.py
+++ quote/forms.py
@@ -1,21 +1,5 @@
 from . import models
-from django import forms
 from django.forms import modelformset_factory
-
-
-class QuoteItemCreateForm(forms.ModelForm):
-    """
-    form for create one quote item
-    """
-
-    class Meta:
-        model = models.QuoteItem
-        fields = [
-            'product',
-            'price',
-            'quantity',
-            'discount',
-        ]
 
 
 QuoteItemCreateFormSet = modelformset_factory(models.QuoteItem, fields=('product', 'price', 'quantity', 'discount'), extra=1)
